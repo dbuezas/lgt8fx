991ea82653e424962a13100b6c8b62ce23fae871
David Buezas <david.buezas@gmail.com>
2019-09-16 20:29:16 +0200

    added screenshot and prepared deploy

--- simlink.sh
+++ simlink.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-rm -r ~/Library/Arduino15/packages/LGT8fx\ Boards/hardware/avr/1.0.2
-ln -s ~/code/lgt8fx/lgt8f  ~/Library/Arduino15/packages/LGT8fx\ Boards/hardware/avr/1.0.2
+mv ~/Library/Arduino15/packages/LGT8fx\ Boards/hardware/avr/1.0.4 ~/Library/Arduino15/packages/LGT8fx\ Boards/hardware/avr/1.0.4_bak
+ln -s ~/code/lgt8fx/lgt8f  ~/Library/Arduino15/packages/LGT8fx\ Boards/hardware/avr/1.0.4
