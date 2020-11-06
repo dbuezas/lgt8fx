c8133f982c0b52b8034ef879b1fd79928ef8a944
SuperUserNameMan <yoann@terminajones.com>
2020-11-06 13:03:33 +0100

    Update eeprom_read.ino

--- lgt8f/libraries/E2PROM/examples/eeprom_read/eeprom_read.ino
+++ lgt8f/libraries/E2PROM/examples/eeprom_read/eeprom_read.ino
@@ -6,6 +6,7 @@
  * This example code is in the public domain.
  */
 
+#define USE_LGT_EEPROM_API
 #include <EEPROM.h>
 
 // start reading from the first byte (address 0) of the EEPROM
@@ -34,9 +35,9 @@ void loop()
   // advance to the next address of the EEPROM
   address = address + 1;
   
-  // there are only 512 bytes of EEPROM, from 0 to 511, so if we're
-  // on address 512, wrap around to address 0
-  if (address == 512)
+  // there are only 1020 bytes of EEPROM, from 0 to 1019, so if we're
+  // on address 1020, wrap around to address 0
+  if (address == EEPROM.size() )
     address = 0;
     
   delay(500);
