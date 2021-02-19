e14d24f84c672401458584075c95a68d826d8485
SuperUserNameMan <yoann@terminajones.com>
2021-02-19 15:53:46 +0100

    better default compatibility with lgt8fx8a/e/d

--- lgt8f/libraries/E2PROM/EEPROM.cpp
+++ lgt8f/libraries/E2PROM/EEPROM.cpp
@@ -1,9 +1,9 @@
 #include "Arduino.h"
 #include "EEPROM.h"
 
+#ifdef __LGT8FX8P__ 
 void lgt_eeprom_init( uint8_t number_of_1KB_pages )
 {
-#ifdef __LGT8FX8P__ // TODO catch LGT8F32p only here
 	switch( number_of_1KB_pages )
 	{
 		case 0: // disable EEPROM emulation
@@ -31,12 +31,13 @@ void lgt_eeprom_init( uint8_t number_of_1KB_pages )
 			ECCR = 0x4C;
 		break;
 	}
-#endif
 }
+#endif
+
 
 int lgt_eeprom_size( bool theoretical )
 {
-#ifdef __LGT8FX8P__ // TODO catch LGT8F32p only here
+#if defined( __LGT8FX8P__ )
 
 	if ( ECCR & 0x40 ) // EEPROM emulation enabled ?
 	{
@@ -48,17 +49,29 @@ int lgt_eeprom_size( bool theoretical )
 	}
 
 	return 0;
+
+#elif defined( __LGT8F88A__ )
+	// The EEPROM is emulated using Flash too. 
+	// Some bytes of the EEPROM are used to store fuse and flags.
+	// Only 504 bytes are actually available for write.
 	
+	return theoretical ? 512 : 504; 
+
 #else
-	// TODO : detect LGT8F8  and return 2048
-	// TODO : detect LGT8F16 and return 4096
 	// TODO : detect LGT8F32d and return ???
 	// TODO : detect LGT8F32e and return ???
-
-	return 1024;
+	
+	(void)(theoretical); // suppress unused parameter warning
+	
+	#ifdef E2END
+		return E2END+1;
+	#else
+		return 1024;
+	#endif
 #endif
 }
 
+#ifdef __LGT8FX8P__
 uint16_t lgt_eeprom_continuous_address_to_real_address( uint16_t address )
 {
 	// we recalculate the address so that we automatically skip 
@@ -73,15 +86,15 @@ uint16_t lgt_eeprom_continuous_address_to_real_address( uint16_t address )
 
 	return address;
 }
+#endif
 
+#ifdef __LGT8FX8P__
 uint8_t lgt_eeprom_read_byte( uint16_t address, bool real_address_mode )
 {
-#ifdef __LGT8FX8P__ // TODO catch LGT8F32p only here
 	if ( ! real_address_mode )
 	{
 		address = lgt_eeprom_continuous_address_to_real_address( address );
 	}
-#endif
 
 	if ( address >= (uint16_t)lgt_eeprom_size( real_address_mode ) ) return 0;
 
@@ -92,20 +105,35 @@ uint8_t lgt_eeprom_read_byte( uint16_t address, bool real_address_mode )
 	__asm__ __volatile__ ("nop" ::);
 	__asm__ __volatile__ ("nop" ::);
 	
-	return EEDR;	
+	return EEDR;
 }
+#else
+uint8_t lgt_eeprom_read_byte( uint16_t address )
+{
+	if ( address >= (uint16_t)lgt_eeprom_size( false ) ) return 0;
+	
+	EEARL = address & 0xff;
+	EEARH = (address >> 8); 
+	 
+	EECR |= (1 << EERE);
+	__asm__ __volatile__ ("nop" ::);
+	__asm__ __volatile__ ("nop" ::);
+	
+	return EEDR;
+}
+#endif
 
+#ifdef __LGT8FX8P__
 void lgt_eeprom_write_byte( uint16_t address, uint8_t value, bool real_address_mode )
 {
-#ifdef __LGT8FX8P__ // TODO catch LGT8F32p only here
+
 	if ( ! real_address_mode )
 	{
 		address = lgt_eeprom_continuous_address_to_real_address( address );
 	}
-#endif
-
+	
 	if ( address >= (uint16_t)lgt_eeprom_size( real_address_mode ) ) return;
-
+	
 	uint8_t	__bk_sreg = SREG;
 
 	// set address & data
@@ -113,12 +141,31 @@ void lgt_eeprom_write_byte( uint16_t address, uint8_t value, bool real_address_m
 	EEARH = (address >> 8);
 	EEDR = value;
 	 
-	cli();	
+	cli();
 	EECR = 0x04;
 	EECR = 0x02;
 	SREG = __bk_sreg;
 }
+#else
+void lgt_eeprom_write_byte( uint16_t address, uint8_t value )
+{
+	if ( address >= (uint16_t)lgt_eeprom_size( false ) ) return;
+	
+	uint8_t	__bk_sreg = SREG;
 
+	// set address & data
+	EEARL = address & 0xff;
+	EEARH = (address >> 8);
+	EEDR = value;
+	 
+	cli();
+	EECR = 0x04;
+	EECR = 0x02;
+	SREG = __bk_sreg;
+}
+#endif
+
+#ifdef __LGT8FX8P__
 void lgt_eeprom_read_block( uint8_t *pbuf, uint16_t address, uint8_t len, bool real_address_mode )
 {
 	uint8_t i;
@@ -129,7 +176,20 @@ void lgt_eeprom_read_block( uint8_t *pbuf, uint16_t address, uint8_t len, bool r
 		*p++ = lgt_eeprom_read_byte(address+i, real_address_mode );
 	}
 }
+#else
+void lgt_eeprom_read_block( uint8_t *pbuf, uint16_t address, uint8_t len )
+{
+	uint8_t i;
 
+	uint8_t *p = pbuf;
+
+	for(i = 0; i < len; i++) {
+		*p++ = lgt_eeprom_read_byte(address+i);
+	}
+}
+#endif
+
+#ifdef __LGT8FX8P__
 void lgt_eeprom_write_block( uint8_t *pbuf, uint16_t address, uint8_t len, bool real_address_mode )
 {
 	uint8_t i;
@@ -140,119 +200,131 @@ void lgt_eeprom_write_block( uint8_t *pbuf, uint16_t address, uint8_t len, bool 
 		lgt_eeprom_write_byte( address+i, *p++, real_address_mode );
 	}
 }
-
-#if defined(__LGT8FX8P__) // TODO catch LGT8F32p only here
-uint32_t lgt_eeprom_read32( uint16_t address )
-{
-	uint32_t dwTmp;
-
-	 EEARL = address & 0xff;
-	 EEARH = (address >> 8);
-	 
-	 EECR |= (1 << EERE);
-	
-	 __asm__ __volatile__ ("nop" ::);
-	 __asm__ __volatile__ ("nop" ::);
-	
-	dwTmp = E2PD0;
-	dwTmp |= ((uint32_t)E2PD1 << 8);
-	dwTmp |= ((uint32_t)E2PD2 << 16);
-	dwTmp |= ((uint32_t)E2PD3 << 24);
-
-	// return data from data register
-	return dwTmp;	
-}
-
-void lgt_eeprom_write32( uint16_t address, uint32_t value )
-{
-	uint8_t __bk_sreg = SREG;
-
-	EEARL = 0;
-	EEDR = value;
-	EEARL = 1;
-	EEDR = value >> 8;
-	EEARL = 2;
-	EEDR = value >> 16;
-	EEARL = 3;
-	EEDR = value >> 24;
-
-	EEARH = address >> 8;
-	EEARL = address & 0xff;
-
-	// Program Mode
-	cli();
-	EECR = 0x44;
-	EECR = 0x42;
-	
-	SREG = __bk_sreg;	
-}
-
-// ----------------------------------------------------------------------
-// public: write bundle of data to E2PROM with SWM mode enable
-// ----------------------------------------------------------------------
-void lgt_eeprom_writeSWM( uint16_t address, uint32_t *pData, uint8_t length )
+#else
+void lgt_eeprom_write_block( uint8_t *pbuf, uint16_t address, uint8_t len )
 {
 	uint8_t i;
-	uint8_t __bk_sreg;
 
-	lgt_eeprom_reset();
-	lgt_eeprom_SWM_ON();
+	uint8_t *p = pbuf;
 
-	EEARH = address >> 8;
-	EEARL = address;
+	for(i = 0; i < len; i++) {
+		lgt_eeprom_write_byte( address+i, *p++ );
+	}
+}
+#endif
 
-	for( i = 0; i < length; i++ ) 
+#if defined(__LGT8FX8P__) 
+	uint32_t lgt_eeprom_read32( uint16_t address )
 	{
-		if( i == (length - 1) )  // the last word
-		{
-			lgt_eeprom_SWM_OFF();
-		}
+		uint32_t dwTmp;
 
-		E2PD0 = (uint8_t) pData[i];
-		E2PD1 = (uint8_t)(pData[i] >> 8);
-		E2PD2 = (uint8_t)(pData[i] >> 16);
-		E2PD3 = (uint8_t)(pData[i] >> 24);
+		 EEARL = address & 0xff;
+		 EEARH = (address >> 8);
+		 
+		 EECR |= (1 << EERE);
+		
+		 __asm__ __volatile__ ("nop" ::);
+		 __asm__ __volatile__ ("nop" ::);
+		
+		dwTmp = E2PD0;
+		dwTmp |= ((uint32_t)E2PD1 << 8);
+		dwTmp |= ((uint32_t)E2PD2 << 16);
+		dwTmp |= ((uint32_t)E2PD3 << 24);
 
-		__bk_sreg = SREG;
+		// return data from data register
+		return dwTmp;	
+	}
+
+	void lgt_eeprom_write32( uint16_t address, uint32_t value )
+	{
+		uint8_t __bk_sreg = SREG;
+
+		EEARL = 0;
+		EEDR = value;
+		EEARL = 1;
+		EEDR = value >> 8;
+		EEARL = 2;
+		EEDR = value >> 16;
+		EEARL = 3;
+		EEDR = value >> 24;
+
+		EEARH = address >> 8;
+		EEARL = address & 0xff;
+
+		// Program Mode
 		cli();
-
 		EECR = 0x44;
 		EECR = 0x42;
+		
 		SREG = __bk_sreg;	
 	}
-}
 
-// ----------------------------------------------------------------------
-// public: read bundle of data to E2PROM with SWM mode enable
-// ----------------------------------------------------------------------
-void lgt_eeprom_readSWM( uint16_t address, uint32_t *pData, uint8_t length )
-{
-	uint8_t i;
-
-	lgt_eeprom_reset();
-	lgt_eeprom_SWM_ON();
-
-	EEARH = address >> 8;
-	EEARL = address;
-
-	for(i = 0; i < length; i++) 
+	// ----------------------------------------------------------------------
+	// public: write bundle of data to E2PROM with SWM mode enable
+	// ----------------------------------------------------------------------
+	void lgt_eeprom_writeSWM( uint16_t address, uint32_t *pData, uint8_t length )
 	{
-		if(i == (length - 1)) // the last word
+		uint8_t i;
+		uint8_t __bk_sreg;
+
+		lgt_eeprom_reset();
+		lgt_eeprom_SWM_ON();
+
+		EEARH = address >> 8;
+		EEARL = address;
+
+		for( i = 0; i < length; i++ ) 
 		{
-			lgt_eeprom_SWM_OFF();
+			if( i == (length - 1) )  // the last word
+			{
+				lgt_eeprom_SWM_OFF();
+			}
+
+			E2PD0 = (uint8_t) pData[i];
+			E2PD1 = (uint8_t)(pData[i] >> 8);
+			E2PD2 = (uint8_t)(pData[i] >> 16);
+			E2PD3 = (uint8_t)(pData[i] >> 24);
+
+			__bk_sreg = SREG;
+			cli();
+
+			EECR = 0x44;
+			EECR = 0x42;
+			SREG = __bk_sreg;	
 		}
-
-		EECR |= (1 << EERE);
-	 	
-		__asm__ __volatile__ ("nop" ::);
-	 	__asm__ __volatile__ ("nop" ::);
-
-		pData[i]  =  (uint32_t)E2PD0;
-		pData[i] |= ((uint32_t)E2PD1 << 8);
-		pData[i] |= ((uint32_t)E2PD2 << 16);
-		pData[i] |= ((uint32_t)E2PD3 << 24);
 	}
-}
+
+	// ----------------------------------------------------------------------
+	// public: read bundle of data to E2PROM with SWM mode enable
+	// ----------------------------------------------------------------------
+	void lgt_eeprom_readSWM( uint16_t address, uint32_t *pData, uint8_t length )
+	{
+		uint8_t i;
+
+		lgt_eeprom_reset();
+		lgt_eeprom_SWM_ON();
+
+		EEARH = address >> 8;
+		EEARL = address;
+
+		for(i = 0; i < length; i++) 
+		{
+			if(i == (length - 1)) // the last word
+			{
+				lgt_eeprom_SWM_OFF();
+			}
+
+			EECR |= (1 << EERE);
+			
+			__asm__ __volatile__ ("nop" ::);
+			__asm__ __volatile__ ("nop" ::);
+
+			pData[i]  =  (uint32_t)E2PD0;
+			pData[i] |= ((uint32_t)E2PD1 << 8);
+			pData[i] |= ((uint32_t)E2PD2 << 16);
+			pData[i] |= ((uint32_t)E2PD3 << 24);
+		}
+	}
 #endif
 
 
