2755eb65308a48c21ea727202d932d05498bc46e
David Buezas <dbuezas@users.noreply.github.com>
2023-01-20 14:15:00 +0100

    Delete simlink.sh

--- simlink.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-mv ~/Library/Arduino15/packages/LGT8fx\ Boards/hardware/avr/1.0.4 ~/Library/Arduino15/packages/LGT8fx\ Boards/hardware/avr/1.0.4_bak
-ln -s ~/code/lgt8fx/lgt8f  ~/Library/Arduino15/packages/LGT8fx\ Boards/hardware/avr/1.0.4
